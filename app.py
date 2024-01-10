9af49773d1ecaa43e33f319de892985ba58ea76a
Sneha Supriya <codingsneha@gmail.com>
2024-01-11 00:35:49 +0530

    update

--- app.py
+++ app.py
@@ -82,7 +82,7 @@ def user_input(user_question):
 
 def main():
     st.set_page_config("Chat PDF")
-    st.header("Chat with PDF using Gemini💁")
+    st.header("Chat with PDF using Gemini!")
 
     user_question = st.text_input("Ask a Question from the PDF Files")
 
